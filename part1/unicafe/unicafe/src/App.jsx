558a5ce214113e5647a434a4f0c951cb2d48286c
MANUEL BALDERAS VICTORIA <manuel.balderas@edu.uag.mx>
2024-06-07 17:12:58 -0600

    Exercise 1.11: unicafe step 6 Completed

--- part1/unicafe/unicafe/src/App.jsx
+++ part1/unicafe/unicafe/src/App.jsx
@@ -3,73 +3,77 @@ import { useState } from 'react'
 
 const Button = ({ clickHandler, text }) => <button onClick={clickHandler}>{text}</button>
 const DisplayTitle = ({ text }) => <h1>{text}</h1>
-const DisplayStatistic = ({ text, quantity }) => <p>{text} - {Math.round(quantity*100)/100}</p>
-const DisplayPercentage = ({ text, quantity }) => <p>{text} - {quantity.toFixed(2)} %</p>
+const DisplayStatistic = ({ text, quantity }) => <tr><th>{text}</th><td>{Math.round(quantity * 100) / 100}</td></tr>
+const DisplayPercentage = ({ text, quantity }) => <tr><th>{text}</th><td>{quantity.toFixed(2)} %</td></tr>
 
-const Statistics = ({clicks}) => {
-  if(clicks.total===0){
+const Statistics = ({ clicks }) => {
+  if (clicks.total === 0) {
     return (
       <p>Currently, there's no feedback given. Please click a button!</p>
     )
   }
   return (
     <>
-    <DisplayTitle text='Statistics' />
-    <DisplayStatistic text="Good" quantity={clicks.good} />
-    <DisplayStatistic text="Neutral" quantity={clicks.neutral} />
-    <DisplayStatistic text="Bad" quantity={clicks.bad} />
-    <DisplayStatistic text="All" quantity={clicks.total} />
-    <DisplayStatistic text="Average" quantity={clicks.average} />
-    <DisplayPercentage text="Positive" quantity={clicks.positive} />
+      <DisplayTitle text='Statistics' />
+      <table>
+        <tbody>
+          <DisplayStatistic text="Good" quantity={clicks.good} />
+          <DisplayStatistic text="Neutral" quantity={clicks.neutral} />
+          <DisplayStatistic text="Bad" quantity={clicks.bad} />
+          <DisplayStatistic text="All" quantity={clicks.total} />
+          <DisplayStatistic text="Average" quantity={clicks.average} />
+          <DisplayPercentage text="Positive" quantity={clicks.positive} />
+        </tbody>
+      </table>
     </>
   )
-  
-} 
+
+}
 
 const App = () => {
   // save clicks of each button to its own state
 
   const [clicks, setClicks] = useState({
-    good:0,
-    neutral:0,
-    bad:0,
-    total:0,
-    average:0,
-    positive:0
+    good: 0,
+    neutral: 0,
+    bad: 0,
+    total: 0,
+    average: 0,
+    positive: 0
   })
-  
+
   const GoodHandler = () => {
-    const uGood = clicks.good+1
-    const uTotal = clicks.total+1  
+    const uGood = clicks.good + 1
+    const uTotal = clicks.total + 1
     setClicks({
-      ...clicks, 
-      good:clicks.good+1,
-      total:clicks.total+1,
-      average:(uGood-clicks.bad)/uTotal,
-      positive:uGood/uTotal*100
+      ...clicks,
+      good: clicks.good + 1,
+      total: clicks.total + 1,
+      average: (uGood - clicks.bad) / uTotal,
+      positive: uGood / uTotal * 100
     })
   }
 
   const NeutralHandler = () => {
-    const uTotal=clicks.total+1
+    const uTotal = clicks.total + 1
     setClicks({
       ...clicks,
       neutral: clicks.neutral + 1,
-      total:clicks.total+1,
-      average:(clicks.good-clicks.bad)/uTotal,
-      positive:clicks.good/uTotal*100
-    })    
-  } 
+      total: clicks.total + 1,
+      average: (clicks.good - clicks.bad) / uTotal,
+      positive: clicks.good / uTotal * 100
+    })
+  }
 
   const BadHandler = () => {
-    const uBad = clicks.bad+1
-    const uTotal = clicks.total+1    
+    const uBad = clicks.bad + 1
+    const uTotal = clicks.total + 1
     setClicks({
       ...clicks,
-      bad:clicks.bad + 1,
-      total:clicks.total+1,
-      average:(clicks.good-uBad)/uTotal,
-      positive:clicks.good/uTotal*100
+      bad: clicks.bad + 1,
+      total: clicks.total + 1,
+      average: (clicks.good - uBad) / uTotal,
+      positive: clicks.good / uTotal * 100
     })
   }
 
