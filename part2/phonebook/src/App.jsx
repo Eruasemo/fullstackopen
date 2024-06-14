794449b4189d6040bf9de2d9e2f65292421a4db7
MANUEL BALDERAS VICTORIA <manuel.balderas@edu.uag.mx>
2024-06-14 01:59:02 -0600

    2.8: The Phonebook Step 3 Completed

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,39 +1,43 @@
 import { useState } from 'react'
 import './App.css'
 
-const DisplayNames = ({ persons }) => persons.map(person => <li key={person.name}>{person.name}</li>)
+const DisplayNames = ({ persons }) => persons.map(person => <li key={person.number}>{person.name} - {person.number}</li>)
+const Input = ({name,value,handler,type}) =><div>{name}: <input type={type} name={name} value={value} onChange={handler} required /></div>
+
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: 123456789 }
   ])
-  const [newName, setNewName] = useState('')
+  const [newName, setNewName] = useState({name:'',number:''})
 
 
   const addPerson = (event) => {
     event.preventDefault()
-    const personObject = {
-      name: newName,
-    }
-    if (persons.some(person => JSON.stringify(person) === JSON.stringify(personObject))) {
-      alert(`${personObject.name} is already on the phonebook.`)
+    const personObject = newName
+    //I decided that I should be able to register two numbers for the same person, but I can't register the same number with different names.
+    if (persons.some(person => JSON.stringify(person.number) === JSON.stringify(personObject.number))) {
+      alert(`${personObject.number} is already on the phonebook.`)
     } else {
       setPersons(persons.concat(personObject));
-      setNewName('')
+      setNewName({name:'',number:''})    
     }
   }
 
   const handleNameChange = (event) => {
-    setNewName(event.target.value)
+    setNewName({...newName,name:event.target.value})
+  }
+
+  const handleNumberChange = (event) =>{
+    setNewName({...newName,number:event.target.value})    
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input name='name' value={newName} onChange={handleNameChange} required />
-        </div>
+      <form onSubmit={addPerson}>        
+        <Input name = 'Name' type='text' value = {newName.name} handler={handleNameChange} />
+        <Input name='Number' type='number' value = {newName.number} handler={handleNumberChange} />
         <div>
           <button type="submit">add</button>
         </div>
